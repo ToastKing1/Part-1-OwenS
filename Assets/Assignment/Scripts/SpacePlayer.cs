9fd30e1144c801912e611a6d38e983d11197adde
ToastKing1 <owenspreadbury1@gmail.com>
2024-01-29 13:53:00 -0500

    player movement is complete. This is a commit just to save changes to be later worked on.

---
+++ Assets/Assignment/Scripts/SpacePlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacePlayer : MonoBehaviour
+{
+
+    public float speed = 2.5f;
+    public GameObject lazer;
+    public Transform lazerSpawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
